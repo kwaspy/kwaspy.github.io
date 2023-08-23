47758a96077960530bd9e7a10534bd2a893d2fff
km <kevin.m.magnussen@gmail.com>
2023-08-23 07:04:06 +0200

    refactor: move function to new module

---
+++ day.js
@@ -0,0 +1,13 @@
+function dayName(date){
+		let daysOfTheWeek = [
+		    "Sunday", 
+		    "Monday", 
+		    "Tuesday",
+		    "Wednesday", 
+		    "Thursday", 
+		    "Friday", 
+		    "Saturday"
+		];
+		return daysOfTheWeek[date.getDay()];
+	    }
+
